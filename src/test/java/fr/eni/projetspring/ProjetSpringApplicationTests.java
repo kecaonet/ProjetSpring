f6586dfe37d4a0063c49a01a3c66dd4bd5daff72
kecaonet <trotin.kelan22190@gmail.com>
2025-06-30 10:16:57 +0200

    firstcommit

---
+++ src/test/java/fr/eni/projetspring/ProjetSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.projetspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
