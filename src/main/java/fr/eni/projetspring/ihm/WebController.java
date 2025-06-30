8b7a7c0d610d8503398978a211249aac1bfe3117
kecaonet <trotin.kelan22190@gmail.com>
2025-06-30 16:55:26 +0200

    kt commit

--- src/main/java/fr/eni/projetspring/ihm/WebController.java
+++ src/main/java/fr/eni/projetspring/ihm/WebController.java
@@ -1,4 +1,27 @@
 package fr.eni.projetspring.ihm;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class WebController {
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/profil")
+    public String profil(){
+        return "profil";
+    }
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+    @GetMapping("/connex")
+    public String connex(){
+        return "redirect:/index";
+    }
+
 }
