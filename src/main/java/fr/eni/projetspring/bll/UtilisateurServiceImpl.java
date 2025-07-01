bddcd8340a97056c2306b623eccfcce7d1026a94
Tklein2025 <tivizio.klein@sfr.fr>
2025-07-01 13:40:38 +0200

    tiv maj UtilisateurServiceImpl

---
+++ src/main/java/fr/eni/projetspring/bll/UtilisateurServiceImpl.java
@@ -0,0 +1,122 @@
+package fr.eni.projetspring.bll;
+
+import fr.eni.projetspring.bo.Utilisateur;
+import fr.eni.projetspring.exceptions.BusinessCode;
+import fr.eni.projetspring.exceptions.BusinessException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UtilisateurServiceImpl implements UtilisateurService {
+    @Override
+    public void ajouterUtilisateur(Utilisateur utilisateur) {
+        /*BusinessException be = new BusinessException();
+        boolean isValid = true;
+        isValid &= validerUtilisateur(utilisateur, be);
+        isValid &= validerUilisateurUnique(utilisateur.getEmail(), utilisateur.getPseudo(), be);
+        isValid &= validerPseudo(utilisateur.getPseudo(), be);
+        isValid &= validerNom(utilisateur.getNom(), be);
+        isValid &= validerPrenom(utilisateur.getPrenom(), be);
+        isValid &= validerEmail(utilisateur.getEmail(), be);
+        isValid &= validerTelephone(utilisateur.getTelephone(), be);
+        isValid &= validerRue(utilisateur.getRue(), be);
+        isValid &= validercodePostal(utilisateur.getCodePostal(), be);
+        isValid &= validerVille(utilisateur.getVille(), be);
+        isValid &= validerMotDePasse(utilisateur.getMotDePasse(),be);
+        isValid &= validerCredit(utilisateur.getCredit(), be);
+        isValid &= validerAdmin(utilisateur.isAdministrateur(), be);
+*/
+    }
+
+
+    @Override
+    public void modifierUtilisateur(int id) {
+
+    }
+
+    @Override
+    public void supprimerUtilisateur(int id) {
+
+    }
+
+    @Override
+    public Utilisateur consulterUtilisateur(int id) {
+        return null;
+    }
+
+    @Override
+    public void desactiverUtilisateur(int id) {
+
+    }
+
+    @Override
+    public List<Utilisateur> consulterUtilisateurs() {
+        return List.of();
+    }
+
+    @Override
+    public Utilisateur consulterCredits(Utilisateur utilisateur) {
+        return null;
+    }
+
+    private boolean validerUtilisateur(Utilisateur u, BusinessException be) {
+        if (u == null) {
+            be.add(BusinessCode.VALIDATION_UTILISATEUR_NULL);
+            return false;
+        }
+        return true;
+    };
+
+
+    /*private boolean validerAdmin(boolean administrateur, BusinessException be) {
+    }
+
+    private boolean validerCredit(int credit, BusinessException be) {
+    }
+
+    private boolean validerMotDePasse(String motDePasse, BusinessException be) {
+    }
+
+    private boolean validerVille(String ville, BusinessException be) {
+    }
+
+    private boolean validercodePostal(String codePostal, BusinessException be) {
+    }
+
+    private boolean validerRue(String rue, BusinessException be) {
+    }
+
+    private boolean validerTelephone(String telephone, BusinessException be) {
+    }
+
+    private boolean validerEmail(String email, BusinessException be) {
+    }
+
+    private boolean validerPrenom(String prenom, BusinessException be) {
+    }
+
+    private boolean validerNom(String nom, BusinessException be) {
+    }
+
+    private boolean validerPseudo(String pseudo, BusinessException be) {
+    }
+
+    private boolean validerUilisateurUnique(String email, String pseudo, BusinessException be) {
+        //Valider que peseudo et email sont uniques
+        try {
+            boolean pseudoExiste = true;
+            boolean emailExiste = true;
+            if (pseudoExiste && emailExiste) {
+                be.add(BusinessCode.VALIDATION_UTILISATEUR_UNIQUE);
+                return false;
+            }
+        } catch (DataAccessException e) {
+            //l'utilisateur existe déjà
+            be.add(BusinessCode.VALIDATION_UTILISATEUR_UNIQUE);
+            return false;
+        }
+        return true;
+    }*/
+
+}
