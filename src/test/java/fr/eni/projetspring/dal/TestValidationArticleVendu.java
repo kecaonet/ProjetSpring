e7f40191810d76af6a7ac7331e75d41eac832ed4
kecaonet <trotin.kelan22190@gmail.com>
2025-07-01 14:51:04 +0200

    14h25 kt

--- src/test/java/fr/eni/projetspring/dal/TestValidationArticleVendu.java
+++ src/test/java/fr/eni/projetspring/dal/TestValidationArticleVendu.java
@@ -1,5 +1,6 @@
 package fr.eni.projetspring.dal;
 
+import fr.eni.projetspring.bo.ArticleVendu;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +17,25 @@ public class TestValidationArticleVendu {
     public void TestReadAll() {
         System.out.println((articleVenduDAO.readAllArticleVendu()));
         System.out.println(utilisateurDAO.readAll());
+    }
+    @Test
+    public void TestInsert() {
+        ArticleVendu articleVendu = new ArticleVendu();
+        articleVendu.setNomArticle("Test");
+        articleVendu.setDescription("test");
+        articleVendu.setDateDebutEncheres("01/07/2025");
+        articleVendu.setDateFinEncheres("10/07/2025");
+        articleVendu.setPrixInitial(100);
+        articleVendu.setUtilisateur(1);
+        articleVendu.setCategorie(1);
+    }
+    @Test
+    public void TestUpdate() {
+
+    }
+    @Test
+    public void TestDelete() {
+
     }
 
 }
