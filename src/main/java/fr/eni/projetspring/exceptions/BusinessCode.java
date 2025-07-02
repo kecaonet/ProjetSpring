951f187454d3b865d84287f4c4dca7d2f0f4ac41
Tklein2025 <tivizio.klein@sfr.fr>
2025-07-02 09:09:14 +0200

    tiv création BusinessCode et ProjetSpringSecurityConfig

--- src/main/java/fr/eni/projetspring/exceptions/BusinessCode.java
+++ src/main/java/fr/eni/projetspring/exceptions/BusinessCode.java
@@ -2,6 +2,6 @@ package fr.eni.projetspring.exceptions;
 
 public class BusinessCode {
         // Clefs de validation des BO
-        public static final String VALIDATION_UTILISATEUR_NULL = "validation.film.null";
-        public static final String VALIDATION_UTILISATEUR_UNIQUE = "validation.film.unique";
+        public static final String VALIDATION_UTILISATEUR_NULL = "validation.utilisateur.null";
+        public static final String VALIDATION_UTILISATEUR_UNIQUE = "validation.utilisateur.unique";
     }
