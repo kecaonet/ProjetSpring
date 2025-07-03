b5bccc18138c8e0cfb7ecdf62c86734f0a0f7f94
adrane2023 <aymerick.drane@gmail.com>
2025-07-03 16:50:16 +0200

    ay Maj messages.properties et BusinessCode

--- src/main/java/fr/eni/projetspring/exceptions/BusinessCode.java
+++ src/main/java/fr/eni/projetspring/exceptions/BusinessCode.java
@@ -5,6 +5,6 @@ public class BusinessCode {
         public static final String VALIDATION_UTILISATEUR_NULL = "validation.utilisateur.null";
         public static final String VALIDATION_UTILISATEUR_UNIQUE = "validation.utilisateur.unique";
         public static final String VALIDATION_PSEUDO_INVALIDE = "validation.pseudo.invalide";
-        public static final String BLL_UTILISATEUR_CREER_ERREUR="bll.avis.creer.erreur";
+        public static final String BLL_UTILISATEUR_CREER_ERREUR="bll.utilisateur.creer.erreur";
 
 }
