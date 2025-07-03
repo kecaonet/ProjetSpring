0516b331fe459e2dd0b862d3d2ec8c5b1810af5e
adrane2023 <aymerick.drane@gmail.com>
2025-07-03 12:02:28 +0200

    ay ajout dans BusinessCode

--- src/main/java/fr/eni/projetspring/exceptions/BusinessCode.java
+++ src/main/java/fr/eni/projetspring/exceptions/BusinessCode.java
@@ -4,4 +4,5 @@ public class BusinessCode {
         // Clefs de validation des BO
         public static final String VALIDATION_UTILISATEUR_NULL = "validation.utilisateur.null";
         public static final String VALIDATION_UTILISATEUR_UNIQUE = "validation.utilisateur.unique";
+        public static final String VALIDATION_PSEUDO_INVALIDE = "validation.pseudo.invalide";
     }
